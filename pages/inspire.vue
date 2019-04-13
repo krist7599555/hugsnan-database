a6d252506940ad854a217f24d737cf96b139919a
krist7599555 <krist7599555@gmail.com>
2019-04-13 16:21:56 +0700

    first commit

---
+++ pages/inspire.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section">
+    <h2 class="title is-3 has-text-grey">
+      "Just start
+      <b-icon icon="rocket" size="is-large"/>"
+    </h2>
+    <b-field>
+      <b-input></b-input>
+    </b-field>
+    <h3 class="subtitle is-6 has-text-grey">
+      Author:
+      <a href="https://github.com/anteriovieira">Antério Vieira</a>
+    </h3>
+  </section>
+</template>
