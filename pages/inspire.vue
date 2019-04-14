0b5a679b7b9f800099c679cfbbe8202a40904639
krist7599555 <krist7599555@gmail.com>
2019-04-14 15:01:18 +0700

    add pug loder

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,15 +1,11 @@
-<template>
-  <section class="section">
-    <h2 class="title is-3 has-text-grey">
-      "Just start
-      <b-icon icon="rocket" size="is-large"/>"
-    </h2>
-    <b-field>
-      <b-input></b-input>
-    </b-field>
-    <h3 class="subtitle is-6 has-text-grey">
-      Author:
-      <a href="https://github.com/anteriovieira">Antério Vieira</a>
-    </h3>
-  </section>
+<template lang='pug'>
+  section.section
+    h2.title.is-3.has-text-grey
+      | "Just start2
+      b-icon(icon='rocket', size='is-large') "
+    b-field
+      b-input
+    h3.subtitle.is-6.has-text-grey
+      | Author:
+      a(href='https://github.com/anteriovieira') Antério Vieira
 </template>
